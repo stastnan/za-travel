6443015eb97443c7951e4c26af36fbc1c4d2590b
Nikola Stastna <82889664+stastnan@users.noreply.github.com>
2024-02-23 16:06:38 +0100

    feat: add destinations wizard step (#10)

--- src/features/ui/AppIconButton.tsx
+++ src/features/ui/AppIconButton.tsx
@@ -3,7 +3,7 @@ import { Button, type SxProps, type Theme } from "@mui/material";
 interface Props {
   isSmall?: boolean;
   children: JSX.Element;
-  sx: SxProps<Theme>;
+  sx?: SxProps<Theme>;
   onClick: () => void;
   "aria-label": string;
 }
@@ -11,14 +11,14 @@ interface Props {
 export default function AppIconButton(props: Props) {
   return (
     <Button
-      variant="outlined"
       onClick={props.onClick}
       aria-label={props["aria-label"]}
+      variant="outlined"
       sx={{
         borderRadius: 2,
         minWidth: "auto",
-        width: props.isSmall ? 34 : 58,
-        height: props.isSmall ? 34 : 58,
+        width: { xs: props.isSmall ? 34 : 48, md: props.isSmall ? 34 : 58 },
+        height: { xs: props.isSmall ? 34 : 48, md: props.isSmall ? 34 : 58 },
         ...props.sx,
       }}
     >
