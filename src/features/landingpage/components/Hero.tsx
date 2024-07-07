ef6c4d3625b5655373e8b8016a67ba1af67472a5
stastnan <nik.stastna@gmail.com>
2024-07-07 19:15:16 +0200

    chore: adjust landing page hero layout

--- src/features/landingpage/components/Hero.tsx
+++ src/features/landingpage/components/Hero.tsx
@@ -9,7 +9,7 @@ import Beach from "../assets/Beach1.jpg";
 import City from "../assets/City1.jpg";
 import MobileVan from "../assets/MobileVan.jpg";
 import Van from "../assets/Van1.jpg";
-import { HEADER_HEIGHT_PX } from "../constants";
+import { HEADER_HEIGHT_PX, HERO_MARGIN_TOP_PX } from "../constants";
 import PlanTripButton from "./PlanTripButton";
 import { Plane } from "./Plane";
 import Users from "./Users";
@@ -26,11 +26,17 @@ export default function Hero({ handleClick }: Props) {
       <Stack
         sx={{
           justifyContent: { xs: "flex-start", md: "space-between" },
-          alignItems: { xs: "center", lg: "center" },
+          alignItems: { xs: "center", lg: "center", xl: "space-between" },
           flexDirection: { xs: "column", md: "row" },
           px: { xs: 2, md: 8, lg: 12.5 },
           mt: { xs: 1, md: 4 },
-          height: { xs: "auto", md: `calc(100vh - ${HEADER_HEIGHT_PX}px)` },
+          maxHeight: {
+            xs: "auto",
+            maxHeight: {
+              xs: "auto",
+              md: `calc(100vh - ${HEADER_HEIGHT_PX}px - ${HERO_MARGIN_TOP_PX}px)`,
+            },
+          },
           gap: 3,
           pb: { xs: 0, md: 4 },
         }}
@@ -38,73 +44,77 @@ export default function Hero({ handleClick }: Props) {
         <Stack
           sx={{
             pb: { xs: 0, md: 4 },
-
-            // maxWidth: { xs: "100%", xl: "40%", lg: "60%", md: "70%" },
-            maxWidth: { xs: "100%", md: "70%", lg: "668px" },
+            width: { xs: "100%", md: "70%", xl: "47%" },
             height: "100%",
           }}
         >
-          {md && <Plane />}
-          {!md && (
-            <Box
-              component="img"
-              src={MobileVan}
-              sx={{ borderRadius: { xs: 6, md: 10 } }}
-            />
-          )}
-          <Typography
-            variant="h1"
+          <Stack
             sx={{
-              fontSize: { xs: "2rem", md: "3.75rem" },
-              lineHeight: { xs: "3rem", md: "4.375rem" },
-              color: (theme) => theme.palette.text.primary,
-              fontWeight: FontWeights.bold,
-              textAlign: { xs: "center", md: "left" },
-              mb: 2,
-              mt: { xs: 3, md: 16.25 },
+              height: "100%",
+              justifyContent: "space-between",
+              width: "100%",
             }}
           >
-            Your Ultimate Trip Companion
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
-              lineHeight: { xs: "1.875rem", md: "1.875rem" },
-              color: (theme) => theme.palette.text.secondary,
-              fontWeight: FontWeights.regular,
-              mb: { xs: 5, md: 5 },
-              textAlign: { xs: "center", md: "left" },
-            }}
-          >
-            {`Welcome to ${APP_NAME} – Your Passport to Seamless Adventures!
+            {md && <Plane />}
+            {!md && (
+              <Box
+                component="img"
+                src={MobileVan}
+                sx={{ borderRadius: { xs: 6, md: 10 } }}
+              />
+            )}
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: { xs: "2rem", md: "3.75rem" },
+                lineHeight: { xs: "3rem", md: "4.375rem" },
+                color: (theme) => theme.palette.text.primary,
+                fontWeight: FontWeights.bold,
+                textAlign: { xs: "center", md: "left" },
+                mb: 2,
+                mt: { xs: 3, md: 16.25 },
+              }}
+            >
+              Your Ultimate Trip Companion
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                lineHeight: { xs: "1.875rem", md: "1.875rem" },
+                color: (theme) => theme.palette.text.secondary,
+                fontWeight: FontWeights.regular,
+                mb: { xs: 5, md: 5 },
+                textAlign: { xs: "center", md: "left" },
+              }}
+            >
+              {`Welcome to ${APP_NAME} – Your Passport to Seamless Adventures!
             Discover, plan, and track your journeys effortlessly with our
             intuitive web application.`}
-            {md && ` Start exploring now!`}
-          </Typography>
+              {md && ` Start exploring now!`}
+            </Typography>
+            <Stack
+              sx={{
+                flexDirection: "row",
+                gap: 2,
+                mb: { xs: 1, md: 0 },
+              }}
+            >
+              <PlanTripButton fullWidth />
 
-          <Stack
-            sx={{
-              flexDirection: "row",
-              gap: 2,
-              mb: { xs: 1, md: 0 },
-            }}
-          >
-            <PlanTripButton fullWidth />
+              {md && (
+                <AppButton variant="outlined" fullWidth onClick={handleClick}>
+                  Learn more
+                </AppButton>
+              )}
+            </Stack>
 
-            {md && (
-              <AppButton variant="outlined" fullWidth onClick={handleClick}>
-                Learn more
-              </AppButton>
-            )}
-          </Stack>
-
-          <Stack
-            sx={{
-              mt: 3,
-              justifyContent: "flex-end",
-              height: "100%",
-            }}
-          >
+            <Stack
+              sx={{
+                mt: 3,
+                justifyContent: { xl: "flex-end" },
+                height: "100%",
+              }}
+            ></Stack>
             <Users />
           </Stack>
         </Stack>
@@ -117,14 +127,18 @@ export default function Hero({ handleClick }: Props) {
             sx={{
               justifyContent: "flex-end",
               alignItems: "flex-start",
-              maxWidth: { xs: "100%", md: "28%", xl: "45%", lg: "25%" },
-              maxHeight: { xs: "auto", md: "calc(100vh - 150px)" },
+              maxWidth: { xs: "100%", md: "28%", xl: "46%", lg: "25%" },
+              maxHeight: {
+                xs: "auto",
+                md: `calc(100vh - ${HEADER_HEIGHT_PX}px - ${HERO_MARGIN_TOP_PX}px)`,
+              },
             }}
           >
             <Stack
               gap={3}
               sx={{
                 alignItems: { xs: "center", md: "flex-start" },
+                height: "100%",
               }}
             >
               <Box
@@ -132,14 +146,14 @@ export default function Hero({ handleClick }: Props) {
                 src={Van}
                 alt="Camping van"
                 borderRadius={10}
-                width="100%"
+                sx={{ width: "100%" }}
               />
               <Box
                 component="img"
                 src={Beach}
                 alt="Palm tree on a beach"
                 borderRadius={10}
-                width="100%"
+                sx={{ width: "100%" }}
               />
             </Stack>
 
@@ -159,6 +173,7 @@ export default function Hero({ handleClick }: Props) {
                   alt="Sunset in Seattle city"
                   sx={{
                     width: "100%",
+                    height: "100%",
                     borderTopRightRadius: "54px",
                     borderBottomRightRadius: "54px",
                   }}
--- src/features/landingpage/constants.ts
+++ src/features/landingpage/constants.ts
@@ -1,2 +1,4 @@
 export const HEADER_HEIGHT_PX = 90;
 export const MOBILE_HEADER_HEIGHT_PX = 76;
+
+export const HERO_MARGIN_TOP_PX = 32;
